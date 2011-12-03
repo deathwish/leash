af3b89d771a790622a4fe87ae94a87497fb74411
Lance Cooper <lance.m.cooper@gmail.com>
2011-12-03 16:45:03 -0600

    Added installation of luarocks from central repositories or remote rock files.
    The dependencies of tir are presently included in the lua_dependencies.

--- lib/actions/setup.sh
+++ lib/actions/setup.sh
@@ -3,5 +3,6 @@
 source ./setup/configuration.sh
 source ./setup/create_directory_structure.sh
 source ./setup/build_and_install_server.sh
+source ./setup/install_rocks.sh
 
 create_profile "mongrel2" $SERVER_DIRECTORY "m2sh start -db config.sqlite -name main" "${RUN_DIRECTORY}/mongrel2.pid"
---
+++ setup/install_rocks.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#
+# Ensure that the given package name or url is locally available.
+# If a package name is given, it is printed, otherwise the url is
+# downloaded to $DOWNLOAD_DIRECTORY and 
+# $DOWNLOAD_DIRECTORY/${PACKAGE_NAME}.rockspec is printed.
+# Example: get_rock_installer_path lua_signal would print lua_signal,
+# and get_rock_installer_path http://example.com/package.luarock would
+# print ${DOWNLOAD_DIRECTORY}/package.luarock
+#
+get_rock_installer_path() {
+	 ROCK_NAME=$1
+	 
+	 echo "${ROCK_NAME}" | grep -e 'http:\/\/.*\/*.luarock' > /dev/null
+	 if [ $? == 0 ]
+	 then
+		  log $BUILD_LOG "Downloading luarock ${ROCK_NAME}"
+		  download_package_source $ROCK_NAME
+		  download_path_for_url $ROCK_NAME
+	 else
+		  echo $ROCK_NAME
+	 fi
+}
+
+while read ROCK_NAME
+do
+	 ROCK_INSTALL_PATH="$(get_rock_installer_path $ROCK_NAME)"
+
+	 log $BUILD_LOG "Installing luarock ${ROCK_INSTALL_PATH}"
+	 checked_command $BUILD_LOG luarocks install $ROCK_INSTALL_PATH
+done < "${DATA_DIRECTORY}/lua_dependencies.txt"
