afd705b0bffed9a4ee5e1f54844570c78dde3991
Lance Cooper <lance.m.cooper@gmail.com>
2011-12-03 17:41:08 -0600

    Use an absolute path for build log, corrects checked_command output redirection.

--- setup/configuration.sh
+++ setup/configuration.sh
@@ -6,4 +6,4 @@ export BUILD_DIRECTORY="${LEASH_DIRECTORY}/build"
 export DATA_DIRECTORY="${LEASH_DIRECTORY}/data"
 
 # Logfiles
-export BUILD_LOG="build.log"
+export BUILD_LOG="${LEASH_DIRECTORY}/build.log"
