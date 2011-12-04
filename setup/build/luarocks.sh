4acefec0455533d065a9fda07a3b049784ae4d40
Lance Cooper <lance.m.cooper@gmail.com>
2011-12-04 16:15:29 -0600

    Set linker flags for luarocks.
    Lua native libraries will use --rpath linking to link libraries,
    which allows for use of rocks in PREFIX without exporting LD_LIBRARY_PATH.
    This is likely linux specific at present.

--- setup/build/luarocks.sh
+++ setup/build/luarocks.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 automake_compile $1 "--force-config --sysconfdir=$1/etc/luarocks --with-lua=$1"
+echo "variables[\"LIBFLAG\"] = \"-shared $(linker_flags ${1})\"" >> "${1}/etc/luarocks/config.lua"
