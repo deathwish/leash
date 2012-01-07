344631761d7f3d94338686b90727fdb700f5ca41
Lance Cooper <lance.m.cooper@gmail.com>
2012-01-07 13:27:05 -0600

    Lua build now explicitly sets option flags and link flags.
    Previously, we relied on the Lua platform target to set these, however
    this did not allow us to set include or link paths, thus we could not
    build against local dependencies.

--- setup/build/lua.sh
+++ setup/build/lua.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
-make linux
-make test
-make install INSTALL_TOP=$PREFIX_DIRECTORY
+#
+# Runs Make with compiler flags setup to match the convention used by the Lua makefile.
+# Read the makefile in question for more details.
+#
+lua_make() {
+	 PREFIX=$1
+	 TARGETS=$2
+
+	 make MYCFLAGS="$(preprocessor_flags $PREFIX) -DLUA_USE_LINUX" \
+		  MYLDFLAGS="$(linker_flags $PREFIX) -ldl -lreadline -lhistory -lncurses" \
+		  INSTALL_TOP=$PREFIX \
+		  $TARGETS
+
+	 return $?
+}
+
+
+cd src
+lua_make $1 all
+cd ..
+lua_make $1 test
+lua_make $1 install
