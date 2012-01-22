802e0631622b70575ff161372081bc94ab2f7c45
Lance Cooper <lance.m.cooper@gmail.com>
2012-01-22 14:36:40 -0600

    Use libnss_tld_mapper to provide custom DNS.

---
+++ lib/configure_dns.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+NSS_TLD_MAPPER_SO="${PREFIX_DIRECTORY}/lib/libnss_tld_mapper.so.2"
+if [ -f $NSS_TLD_MAPPER_SO ]
+then
+	export LD_PRELOAD="${NSS_TLD_MAPPER_SO} ${LD_PRELOAD}"
+	export TLD_MAPPER_TLD='.dev'
+	export TLD_MAPPER_SERVICES=`sed -n 's/^hosts: *\(.*\)$/\1/p' /etc/nsswitch.conf`
+fi
